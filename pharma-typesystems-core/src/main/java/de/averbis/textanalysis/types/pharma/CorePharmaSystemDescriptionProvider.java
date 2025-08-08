6ef02b80f8ce2509a9a5da9dc327abbde52495ec
Richard Eckart de Castilho <richard.eckart@averbis.com>
2025-08-08 12:50:41 +0200

    Issue #17: Resolve split package across modules
    - Added parent module
    - Added BOM module
    - Changed legacy module to just redirect (import) the new type systems
    - Added UIMA type system SPI
    - Moved type systems to new consolidated module
    - New module is an OSGI bundle
    - Updated TS imports from core types to modern package names

---
+++ pharma-typesystems-core/src/main/java/de/averbis/textanalysis/types/pharma/CorePharmaSystemDescriptionProvider.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2022 Averbis GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.averbis.textanalysis.types.pharma;
+
+import static aQute.bnd.annotation.Resolution.OPTIONAL;
+
+import org.apache.uima.spi.TypeSystemProvider;
+import org.apache.uima.spi.TypeSystemProvider_ImplBase;
+
+import aQute.bnd.annotation.spi.ServiceProvider;
+
+@ServiceProvider(value = TypeSystemProvider.class, resolution = OPTIONAL)
+public class CorePharmaSystemDescriptionProvider extends TypeSystemProvider_ImplBase {
+
+	public CorePharmaSystemDescriptionProvider() {
+
+		setTypeSystemLocations( //
+				"AdverseEventTypeSystem.xml", //
+				"IdmpTypeSystem.xml", //
+				"Module3TypeSystem.xml", //
+				"PharmaConceptTypeSystem.xml", //
+				"SmpcSectionTypeSystem.xml", //
+				"SmpcTypeSystem.xml");
+	}
+}
